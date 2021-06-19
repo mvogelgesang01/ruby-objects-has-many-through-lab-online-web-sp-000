f9b74a528238aea4120b824c485a766ef2e8fa4d
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-06-19 20:12:18 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,57 +1,23 @@
 class Genre
-
-
-
-
-  attr_accessor :name, :song, :artist
-
-  @@all = []
-
-
-
+  attr_accessor :name
 
   def initialize(name)
-
     @name = name
-
-    @@all << self
-
+    @songs = []
   end
 
-
-
-
-  def self.all
-
-    @@all
-
+  def add_song(song)
+    @songs << song
+    song.genre = self
   end
-
-
-
-
-  def new_song(name, artist)
-
-    Song.new(name, artist, self)
-
-  end
-
-
-
 
   def songs
-
-    Song.all.select {|song| song.genre == self}
-
+    @songs
   end
-
-
-
 
   def artists
-
-    songs.collect {|song| song.artist}
-
+    @songs.collect do |song|
+      song.artist
+    end
   end
-
-end 
+end
