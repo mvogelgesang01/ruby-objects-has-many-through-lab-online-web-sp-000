7e734ed53530ff7acf16dac2981358a25f72bc0d
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-06-19 20:11:52 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,28 +1,23 @@
-class Artist
-
+lass Artist
   attr_accessor :name
-  @@all = []
 
   def initialize(name)
     @name = name
-    @@all << self
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
   end
 
   def songs
-    Song.all.select {|song| song.artist == self}
+    @songs
   end
 
   def genres
-    songs.map {|song| song.genre}
+    @songs.collect do |song|
+      song.genre
+    end
   end
-
-  def self.all
-    @@all
-  end
-
-  def new_song(song, genre)
-    Song.new(name, self, genre)
-  end
-
-
 end
